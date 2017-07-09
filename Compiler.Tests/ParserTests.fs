8c7002b6a70e9087a5920719f5b610540b3918f0
Karol Etrych <karol.etrych@gmail.com>
2017-07-09 22:13:12 +0200

    add myspiritisweak flag to expecto

--- Compiler.Tests/ParserTests.fs
+++ Compiler.Tests/ParserTests.fs
@@ -5,15 +5,7 @@ open Compiler.Parser
 
 [<Tests>]
 let tests =
-  testList "samples" [
-    testCase "universe exists" <| fun _ ->
-      let subject = true
-      Expect.isTrue subject "I compute, therefore I am."
-
-    testCase "should fail" <| fun _ ->
-      let subject = false
-      Expect.isTrue subject "I should fail because the subject is false."
-
+  testList "parser" [
     testCase "parser" <| fun _ ->
-    Expect.equal "a" (Compiler.Parser.parse "a") "tests work"
-  ]
+      Expect.equal "a" (Compiler.Parser.parse "a") "tests work"
+]
