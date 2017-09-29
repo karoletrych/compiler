aa05dee6c3e6bba4b29d44ebfea6c3fe12daead7
Karol Etrych <karol.etrych@gmail.com>
2017-09-29 13:02:54 +0200

    ...

--- Compiler/Result.fs
+++ Compiler/Result.fs
@@ -15,6 +15,7 @@ type Result<'TSuccess, 'TError> =
 | Failure of Failure<'TError> list
 
 let succeed x = Success (x, []) 
+let fail error = Failure [error]
 let succeedUnit = Success ((), []) 
 
 let either fSuccess fFailure = function
@@ -35,6 +36,33 @@ let bind f result =
         Failure errs 
     result |> either fSuccess fFailure 
 
+let addResults r1 r2 = 
+     match (r1, r2) with
+     | Success (s1, errors1), Success (s2, errors2) -> Success (s1, errors1 @ errors2)
+     | Failure f1, Success _  -> Failure f1
+     | Success _ , Failure f2 -> Failure f2
+     | Failure f1, Failure f2 -> Failure (f1 @ f2)
 
+let (&&=) v1 v2 = 
+    addResults v1 v2
 let (>>=) twoTrackInput switchFunction =
     bind switchFunction twoTrackInput
+
+// type Type = {
+//     Name : string;
+//     Base : Type option;
+// }
+// let checktypes ast = 
+//     match ast with
+//     | Some t ->  fail (CannotResolveType ast)
+//     | None -> succeedUnit
+
+// let checktypes2 ast = 
+//     match ast with
+//     | Some t ->  fail (CannotResolveType ast)
+//     | None -> succeedUnit
+
+// checktypes None
+// &&= checktypes2 (Some {Name = "AAA"; Base = None})
+// &&= checktypes2 (Some {Name = "CCC"; Base = None})
+// &&= checktypes None
