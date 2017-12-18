42e6787768024c2805fdd8714c3652b9a5a8837f
Karol Etrych <karol.etrych@gmail.com>
2017-12-18 02:47:38 +0100

    fix todo comment in semantic check

--- src/Compiler/SemanticCheck.fs
+++ src/Compiler/SemanticCheck.fs
@@ -5,20 +5,12 @@ open TypeInference
 open Ast
 open AstProcessing
 
-// Value declaration i variableDeclaration.FullDeclaration type matches expressionType
-// Declarations: variable was not defined
 // Operator jest aplikowalny dla typu
 // Value is not being assigned after initialization
-// Variable already defined
-// Identifier not defined
 // Invalid argument
-// Wrong number of arguments
 // Break -> no enclosing loop
-// Function already defined.
 // No entry point (main)
 // Sprawdzanie cyklicznego dziedziczenia
-// Nazwy zmiennych w ramach funkcji (nie scopea) są unikalne
-// argumenty dla operatorów są poprawne
 // if(p == 0) return 0; else if(p == 1) return "1"; else if(p == 2) return 2.0;
 // sprawdzenie wywolan konstruktorow klas bazowych
 // confilicting module, class, function, variable, field name
